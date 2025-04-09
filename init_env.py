80bdd3e49c75fa07168d25c8847efe1fd53641fc
Su <nostarsbutmyeyes@gmail.com>
2025-04-10 00:14:39 +0800

    Add initial project setup with environment configuration and dependencies

---
+++ init_env.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+
+
+def _set_env(key: str):
+    load_dotenv()
--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from langgraph-simple-chatbot!")
+from init_env import _set_env
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def load_doc_from_web(urls: list[str]) -> list[Document]:
+    loader = WebBaseLoader(urls)
+    docs = loader.load()
+    return docs
+
+
+def splict_docs(docs: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=100, chunk_overlad=50
+    )
+    text_splitter.split_documents(docs)
+
+
+def add_to_vector_store(docs: list[Document]):
+    vectorstore = Chroma.from_documents(
+        documents=docs,
+        embedding=OpenAIEmbeddings(),
+        collection_name="rag_chroma",
+    )
+
+    return vectorstore.as_retriever()
 
 
 if __name__ == "__main__":
-    main()
+    _set_env("OPENAI_API_KEY")
+    urls = [
+        "https://langchain-ai.github.io/langgraph/tutorials/rag/langgraph_agentic_rag"
+    ]
+    docs = load_doc_from_web(urls)
+    splict_docs(docs)
+    add_to_vector_store(docs)
